b166ce2c1cdf7c90acf21d64c70d9215c0fca67d
abdallahalhajahmed <abdallah98fc@gmail.com>
2026-01-24 10:53:52 +0000

    Index.html update

--- src/main/java/com/sparta/spa_api/controller/WebTrainerController.java
+++ src/main/java/com/sparta/spa_api/controller/WebTrainerController.java
@@ -1,10 +1,7 @@
 package com.sparta.spa_api.controller;
 
 import com.sparta.spa_api.dtos.CourseDTO;
-import com.sparta.spa_api.dtos.StudentDTO;
 import com.sparta.spa_api.dtos.StudentMapper;
-import com.sparta.spa_api.dtos.TrainersDTO;
-import com.sparta.spa_api.entities.Course;
 import com.sparta.spa_api.entities.Student;
 import com.sparta.spa_api.services.CourseService;
 import com.sparta.spa_api.services.TrainerService;
@@ -13,11 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/trainers")
@@ -31,13 +25,11 @@ public class WebTrainerController {
         this.studentMapper = studentMapper;
     }
 
-
-
     // Spring creates the Model
     // Controller assigns values to attributes within that model
     // Thymeleaf reads these
     // Model is discarded after the response
-    @GetMapping
+    @GetMapping("/")
     public String trainersHomePage(Model model){
         List<Student> students = courseService.getStudentsByCourseId(1);
         List<CourseDTO> courses = courseService.getAllCourses();
