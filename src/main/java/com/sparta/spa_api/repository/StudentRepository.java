c51971ba32f22c2ce7aaca8af1afcddaed8ccec9
Wajiha Alam <wajiha1905@gmail.com>
2026-02-02 14:14:18 +0000

    Final Push

--- src/main/java/com/sparta/spa_api/repository/StudentRepository.java
+++ src/main/java/com/sparta/spa_api/repository/StudentRepository.java
@@ -1,25 +1,3 @@
-//package com.sparta.spa_api.repository;
-//
-//import com.sparta.spa_api.entities.Spartan;
-//import com.sparta.spa_api.entities.Student;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//@RepositoryRestResource(exported = false)
-//
-//public interface StudentRepository extends JpaRepository<Student,Integer> {
-//    boolean hasGraduated(boolean hasGraduated);
-//
-//    // In StudentRepository
-//    Optional<Student> findByEmail(String email);
-//
-//    Optional<Object> findBySpartan(Spartan spartan);
-//}
-
 package com.sparta.spa_api.repository;
 
 import com.sparta.spa_api.entities.Spartan;
@@ -32,5 +10,6 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     Optional<Student> findBySpartanEmail(String email);
 
     Optional<Student> findBySpartan(Spartan spartan);
-}
 
+    Optional<Student> findBySpartan_Email(String name);
+}
